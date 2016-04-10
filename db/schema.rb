e2441c0af86d7c2ac4742f4cde80717e223c5dcb
bitnami <bitnami@ubuntu.defaultdomain>
2016-04-10 05:37:45 +0000

    rake db:migrate RAILS_ENV=development

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160410041922) do
+ActiveRecord::Schema.define(version: 20160410051752) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string   "name"
+    t.string   "title"
+    t.text     "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
